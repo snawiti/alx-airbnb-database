858930aec1a4e048f17b896c4f92cb51e60fbcea
snawiti <snawiti@yahoo.com>
2025-11-12 10:10:01 +0200

    UPDATED THE ROW_NUMBER

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,23 +9,23 @@ GROUP BY u.id, u.name
 ORDER BY total_bookings DESC;
 
 
--- 2️⃣ Rank properties based on the total number of bookings they have received
+-- 2️⃣ ROW_NUMBER properties based on the total number of bookings they have received
 SELECT
     p.id AS property_id,
     p.name AS property_name,
     COUNT(b.id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_ROW_NUMBER
 FROM properties p
 LEFT JOIN bookings b ON p.id = b.property_id
 GROUP BY p.id, p.name
-ORDER BY booking_rank;
+ORDER BY booking_ROW_NUMBER;
 
 SELECT
     p.id AS property_id,
     p.name AS property_name,
     COUNT(b.id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_ROW_NUMBER
 FROM properties p
 LEFT JOIN bookings b ON p.id = b.property_id
 GROUP BY p.id, p.name
-ORDER BY booking_rank;
+ORDER BY booking_ROW_NUMBER;
